900284f630d2e04becdb6c1a1512e0c29c915d44
Harish Soni <hksoni.9312@gmail.com>
2020-05-31 10:15:24 +0530

    Added the Profile section.

--- dealers/src/Custom/iSidebar.js
+++ dealers/src/Custom/iSidebar.js
@@ -1,16 +1,8 @@
-import React from 'react';
-import { Sidebar } from 'react-admin';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Sidebar } from "react-admin";
+import { makeStyles } from "@material-ui/core/styles";
 
-function iSidebar(props){
-
-    return (
-        <Sidebar {...props}>
-		<div>
-			<span>Jon Snow</span>
-			{props.children}
-		</div>
-        </Sidebar>
-    );
-};
+function iSidebar(props) {
+  return <Sidebar {...props}></Sidebar>;
+}
 export default iSidebar;
