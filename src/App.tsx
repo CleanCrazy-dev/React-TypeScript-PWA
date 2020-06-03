0635a473139a2af7e7aabe114ecb7ae848a24910
Harish Soni <hksoni.9312@gmail.com>
2020-06-03 20:06:47 +0530

    Added the customer screen and done the lighthouse configs

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import { HomePage } from "./pages/Home";
-import { Support } from "./pages/support";
-import { store } from "./store/Store";
-import { BuyOrders } from "./pages/BuyOrders";
 import { LoginScreen } from "./pages/account/Login";
+import { BuyOrders } from "./pages/BuyOrders";
+import { Customers } from "./pages/Customers";
+import { HomePage } from "./pages/Home";
 import { Inventory } from "./pages/Inventory";
-import { Profile } from "./pages/Profile";
 import { Leads } from "./pages/Leads";
 import { AddNewLead } from "./pages/Leads/AddLead";
-import { Customers } from "./pages/Cutomers";
+import { Profile } from "./pages/Profile";
+import { Support } from "./pages/support";
+import { store } from "./store/Store";
+import { AddNewCustomer } from "./pages/Customers/AddNewCustomer";
 
 class App extends React.Component {
   public render() {
     return (
       <Provider store={store}>
         <Router>
+          
           <Switch>
             <Route path="/" exact={true} component={LoginScreen} />
             <Route path="/home" exact={true} component={HomePage} />
@@ -29,6 +31,11 @@ class App extends React.Component {
               component={AddNewLead}
             />
             <Route path="/customers" exact={true} component={Customers} />
+            <Route
+              path="/customer/add-new-customer"
+              exact={true}
+              component={AddNewCustomer}
+            />
             <Route path="/transactions" exact={true} component={Support} />
             <Route path="/rto-process" exact={true} component={Support} />
             <Route path="/buy-orders" exact={true} component={BuyOrders} />
