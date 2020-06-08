51d4645f917c5f39e3facd55d42ee7447db166b6
Harish Soni <hksoni.9312@gmail.com>
2020-06-08 23:47:40 +0530

    Added check for the localhost to hide store in prod.

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,11 +1,12 @@
 import { reducers } from "../reducers/CombinedReducers";
 import { compose, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { isLocalhost } from "src/state/Utility";
 
 function configureStore() {
-  console.log(">> process", process);
   const composeEnhancers =
-    window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] || compose;
+    (isLocalhost() && window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"]) ||
+    compose;
   const storeData = createStore(
     reducers,
     composeEnhancers(applyMiddleware(...getMiddlewares()))
